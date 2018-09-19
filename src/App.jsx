88c370a5229fd1f851140f7df13c20dea1658955
chris <braunsie@gmail.com>
2018-09-18 22:50:16 -0400

    Added actionless 'Generate' Button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import StructureBuilder from './StructureBuilder/StructureBuilder';
 
 import './App.css';
+import { Button } from '@material-ui/core';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         To get started, specify the number of topics, producers, and consumers.
       </p>
       <StructureBuilder/>
+      <Button variant="outlined" color="primary">Generate</Button>
     </div>
   )
 };
